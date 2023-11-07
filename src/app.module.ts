61294bdde0d42341ee8351a2de6f3a6e8738e1fb
hangju0610 <phj950610@naver.com>
2023-11-07 20:42:12 +0900

    Feat: Add TypeOrmModule Config TypeOrmModule Config 세팅 진행 .env 파일 설정 후 gitignore 추가 진행

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,31 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const obj: TypeOrmModuleOptions = {
+          type: 'postgres',
+          host: configService.get<string>('POSTGRES.HOST'),
+          port: configService.get<number>('POSTGRES.PORT'),
+          database: configService.get<string>('POSTGRES.DATABASE'),
+          username: configService.get<string>('POSTGRES.USERNAME'),
+          password: configService.get<string>('POSTGRES.PASSWORD'),
+          autoLoadEntities: true,
+          synchronize: true,
+          logging: true,
+        };
+        return obj;
+      },
+    }),
+    UserModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
