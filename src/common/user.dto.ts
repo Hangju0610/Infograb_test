2e5c9d64625946768583ec8e099da43deb7bcf67
hangju0610 <phj950610@naver.com>
2023-11-07 21:54:41 +0900

    Feat: Add Validator to UserDto UserDto에 validator를 적용

--- src/common/user.dto.ts
+++ src/common/user.dto.ts
@@ -1,5 +1,12 @@
+import { IsEmail, IsString, IsUUID } from 'class-validator';
+
 export class UserDto {
+  @IsUUID(4)
   id: string;
+
+  @IsEmail()
   email: string;
+
+  @IsString()
   password: string;
 }
