8ce96da9e8d7017962c5d0a8c52fd8e7d0a74b6c
hangju0610 <phj950610@naver.com>
2023-11-07 22:37:13 +0900

    Feat: Add CreateUserDto, LoginUserDto CreateUserDto와 LoginUserDto를 생성 Common과 구별하기 위해 제작

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller()
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+  @Get('login')
+  async getLogin() {}
+
+  @Post('login')
+  async postLogin() {}
+
+  @Post('logout')
+  async postLogout() {}
+
+  @Post('signup')
+  async postSignup() {}
+}
--- src/common/user.dto.ts
+++ src/common/user.dto.ts
@@ -1,9 +1,27 @@
 import { IsEmail, IsString, IsUUID } from 'class-validator';
 
-export class UserDto {
+export class CommonUserDto {
   @IsUUID(4)
   id: string;
 
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  password: string;
+}
+export class CreateUserDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  password: string;
+
+  @IsString()
+  confirmPassword: string;
+}
+
+export class loginUserDto {
   @IsEmail()
   email: string;
 
