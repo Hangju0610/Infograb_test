a95f897601ebf22a924a0040bd532af17946841c
hangju0610 <phj950610@naver.com>
2023-11-07 21:47:08 +0900

    Feat: Add userdto Common 폴더에 UserDto를 만들어, validate를 할 수 있도록 하기 위함

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,17 +2,17 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entity/user.entity';
 import { Repository } from 'typeorm';
+import { UserDto } from 'src/common/user.dto';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User) private readonly userRepository: Repository<User>,
   ) {}
-  async findUser(email: string) {
+  async findUser(email: string): Promise<UserDto> {
     const user = this.userRepository.findOneBy({ email });
     return user;
   }
 
   async createUser() {}
-  // 샤워중
 }
